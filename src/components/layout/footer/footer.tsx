a3e1c620c129d9f6fed7cbd3e6a215f1eb12a99a
Luis Roque <luisfroquez@gmail.com>
2026-01-18 13:01:17 -0300

    refactor: update Footer component layout and styling for improved responsiveness

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex flex-col md:flex-row text-sm p-8 leading-none bg-gray-200 justify-between items-center gap-2">
+    <div className="flex justify-center items-center text-xs p-4 bg-gray-200">
       <p>SIIM Ingeniería © 2023-2026</p>
     </div>
   );
